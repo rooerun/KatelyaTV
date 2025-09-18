43eb2d5e786f68df954f114b0cbbcda853350de6
mirror20241221 <mirror20241221@outlook.com>
2025-09-18 18:05:59 +0800

    Delete src/app/api/test/simple/route.ts

--- src/app/api/test/simple/route.ts
+++
@@ -1,7 +0,0 @@
-export const runtime = 'edge';
-
-export async function GET(req: Request) {
-  return new Response(JSON.stringify({ msg: 'Hello from Edge!' }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
